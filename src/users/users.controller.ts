9fc33576c2fae300373e9c05318ee266b08dfab1
OrNixz <ramadhanafif172@gmail.com>
2024-03-03 15:16:41 +0700

    Add user creation functionality with dto validation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Post()
+  createUser(@Body() body: CreateUserDto) {
+    return body;
+  }
+}
