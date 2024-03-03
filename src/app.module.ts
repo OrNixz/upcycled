b4cc0335999a3ff9c61fc06ee8f6bcdb55347420
OrNixz <ramadhanafif172@gmail.com>
2024-03-03 11:06:46 +0700

    Implement item entity, module, and integration with appmodule, and view the results of entities in the database

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,14 @@ import { UsersModule } from './users/users.module';
 import { ItemsModule } from './items/items.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/user.entity';
+import { Item } from './items/item.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db.sqlite',
-      entities: [User],
+      entities: [User, Item],
       synchronize: true,
     }),
     UsersModule,
---
+++ src/items/item.entity.ts
@@ -0,0 +1,22 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Item {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  price: number;
+
+  @Column()
+  location: string;
+
+  @Column()
+  category: string;
+}
