78c258293da215f6d3d19687d7c7ceaff84c2e4f
OrNixz <ramadhanafif172@gmail.com>
2024-03-04 10:50:25 +0700

    Update user creation functionality with dependency injection and repository interaction

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private userService: UsersService) {}
+
   @Post()
   createUser(@Body() body: CreateUserDto) {
-    return body;
+    this.userService.create(body.name, body.email, body.password);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  create(name: string, email: string, password: string) {
+    const user = this.userRepository.create({ name, email, password });
+    return this.userRepository.save(user);
+  }
+}
