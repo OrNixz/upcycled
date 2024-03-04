1b52bd442d799f37d0ebe900b8285cdbc8b9e9f7
OrNixz <ramadhanafif172@gmail.com>
2024-03-04 13:35:20 +0700

    Add user retrieval methods (findAll and findOneBy)

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -9,8 +9,16 @@ export class UsersService {
     @InjectRepository(User) private userRepository: Repository<User>,
   ) {}
 
+  findAll() {
+    return this.userRepository.find();
+  }
+
   create(name: string, email: string, password: string) {
     const user = this.userRepository.create({ name, email, password });
     return this.userRepository.save(user);
+  }
+
+  findOneBy(id: number) {
+    return this.userRepository.findOneBy({ id });
   }
 }
