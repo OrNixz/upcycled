d6a4236bfcc8b4707a7c6b582805276376c1d684
OrNixz <ramadhanafif172@gmail.com>
2024-03-05 11:11:03 +0700

    Implement user update endpoint with partial updates and dto validation

---
+++ src/users/dtos/update-user.dto.ts
@@ -0,0 +1,15 @@
+import { IsEmail, IsOptional, IsString } from "class-validator";
+
+export class UpdateUserDto {
+    @IsString()
+    @IsOptional()
+    name: string;
+
+    @IsEmail()
+    @IsOptional()
+    email: string;
+
+    @IsString()
+    @IsOptional()
+    password: string;
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,11 +4,13 @@ import {
   Delete,
   Get,
   Param,
+  Patch,
   Post,
   Query,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
+import { UpdateUserDto } from './dtos/update-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -27,6 +29,11 @@ export class UsersController {
   @Get('/:id')
   findUser(@Param('id') id: string) {
     return this.usersService.findOneBy(parseInt(id));
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.usersService.update(parseInt(id), body);
   }
 
   @Delete('/:id')
