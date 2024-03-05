f6b48cf5e55db70ac05465a1c9e3b421e2983d0b
OrNixz <ramadhanafif172@gmail.com>
2024-03-05 10:59:48 +0700

    Implement user deletion endpoint

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,23 +1,36 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
-  constructor(private userService: UsersService) {}
+  constructor(private usersService: UsersService) {}
 
   @Get()
   findAllUsers(@Query('email') email: string) {
-    return this.userService.findAll(email);
+    return this.usersService.findAll(email);
   }
 
   @Post()
   createUser(@Body() body: CreateUserDto) {
-    return this.userService.create(body.name, body.email, body.password);
+    return this.usersService.create(body.name, body.email, body.password);
   }
 
   @Get('/:id')
   findUser(@Param('id') id: string) {
-    return this.userService.findOneBy(parseInt(id));
+    return this.usersService.findOneBy(parseInt(id));
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id));
   }
 }
