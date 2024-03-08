22f9c8c03bc55382da9b6821a6475b0a57e412e2
OrNixz <ramadhanafif172@gmail.com>
2024-03-08 15:07:25 +0700

    Create user registration with email check

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,7 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Injectable()
 export class AuthService {
   constructor(private userService: UsersService) {}
+
+  async register(name: string, email: string, password: string) {
+    const users = await this.userService.findAll(email);
+    if (!users.length) {
+      throw new BadRequestException('Email in use');
+    }
+  }
 }
