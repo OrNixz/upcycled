649d67962c267734db0858e53956d3cf36b0cd87
OrNixz <ramadhanafif172@gmail.com>
2024-03-08 17:13:42 +0700

    Implement secure user registration with password hashing and fix mistyping code

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,5 +1,9 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { promisify } from 'util';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+
+const scrypt = promisify(_scrypt);
 
 @Injectable()
 export class AuthService {
@@ -7,8 +11,13 @@ export class AuthService {
 
   async register(name: string, email: string, password: string) {
     const users = await this.userService.findAll(email);
-    if (!users.length) {
+    if (users.length) {
       throw new BadRequestException('Email in use');
     }
+
+    const salt = randomBytes(8).toString('hex');
+    const hash = (await scrypt(password, salt, 64)) as Buffer;
+    const result = salt + '.' + hash.toString('hex');
+    
   }
 }
