55d45bb3d66ae4bb73bc0d68f202026208fbc6d5
OrNixz <ramadhanafif172@gmail.com>
2024-03-15 18:08:17 +0700

    Implement basic AuthController tests with login, register, and logout

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { UsersService } from '../users/users.service';
 import { AuthService } from './auth.service';
+import { User } from '../users/user.entity';
 
 describe('AuthController', () => {
   let controller: AuthController;
@@ -10,7 +11,14 @@ describe('AuthController', () => {
 
   beforeEach(async () => {
     fakeUsersService = {};
-    fakeAuthService = {};
+    fakeAuthService = {
+      register: (name: string, email: string, password: string) => {
+        return Promise.resolve({ id: 1, name, email, password } as User);
+      },
+      login: (email: string, password: string) => {
+        return Promise.resolve({ id: 1, email, password } as User);
+      },
+    };
 
     const module: TestingModule = await Test.createTestingModule({
       providers: [
@@ -25,5 +33,40 @@ describe('AuthController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should return a user on login', async () => {
+    const session = { userId: -10 };
+    const user = await controller.login(
+      { email: 'johndoe@gmail.com', password: 'password' },
+      session,
+    );
+    expect(user).toEqual({
+      id: 1,
+      email: 'johndoe@gmail.com',
+      password: 'password',
+    });
+    expect(session.userId).toEqual(1);
+  });
+
+  it('should return a user on register', async () => {
+    const session = { userId: -10 };
+    const user = await controller.register(
+      { name: 'John Doe', email: 'johndoe@gmail.com', password: 'password' },
+      session,
+    );
+    expect(user).toEqual({
+      id: 1,
+      name: 'John Doe',
+      email: 'johndoe@gmail.com',
+      password: 'password',
+    });
+    expect(session.userId).toEqual(1);
+  });
+
+  it('should clear session on logout', () => {
+    const session = { userId: 1 };
+    controller.logout(session);
+    expect(session.userId).toBeNull();
   });
 });
