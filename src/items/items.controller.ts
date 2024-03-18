1d2409566ff8d2e481c2eee6a446d5e860684dcd
OrNixz <ramadhanafif172@gmail.com>
2024-03-18 14:27:48 +0700

    Secure item creation endpoint with authentication and update create method

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,12 +1,14 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { CreateItemDto } from './dtos/create-item.dto';
 import { ItemsService } from './items.service';
+import { AuthGuard } from '../guards/auth.guard';
 
 @Controller('items')
 export class ItemsController {
   constructor(private itemsService: ItemsService) {}
 
   @Post()
+  @UseGuards(AuthGuard)
   createItem(@Body() body: CreateItemDto) {
     return this.itemsService.create(body);
   }
--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Item } from './item.entity';
+import { Repository } from 'typeorm';
+import { CreateItemDto } from './dtos/create-item.dto';
 
 @Injectable()
-export class ItemsService {}
+export class ItemsService {
+  constructor(
+    @InjectRepository(Item) private itemRepository: Repository<Item>,
+  ) {}
+
+  create(item: CreateItemDto) {
+    const newItem = this.itemRepository.create(item);
+    return this.itemRepository.save(newItem);
+  }
+}
